6dfe52acaa125a1b05d26573b37c2e87a42c119d
Henrique Santos <henrique.144@outlook.com>
2021-08-22 20:46:12 -0300

    Concluido

---
+++ FilmesAPI/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class CreateFilmeDto
+    {
+        [Required(ErrorMessage = "O campo Titulo é obrigatório.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo Diretor é obrigatório.")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "O campo Genero deve ter no máximo 30 caracteres.")]
+        public string Genero { get; set; }
+
+        [Range(1, 200, ErrorMessage = "A Duracao deve ter no minimo 1 e no máximo 200 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
