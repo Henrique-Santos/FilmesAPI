4ca07b02333e5f2c50692f5df67f160d21be5a8b
Henrique Santos <henrique.144@outlook.com>
2021-10-11 10:31:58 -0300

    UsuariosApi

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class CadastroController : ControllerBase
+    {
+        public CadastroController()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            return Ok();
+        }
+    }
+}
